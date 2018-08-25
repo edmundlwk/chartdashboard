9f4c101a847674d6951079a3d9c50e2d69adf76a
edmundlwk <42687032+edmundlwk@users.noreply.github.com>
2018-08-25 10:12:03 +0800

    Add files via upload

---
+++ Chartdashboard/Chart/assets/scripts/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4oTgfGSGxntnpc1SsWXgmXi0pv0Va_KA",
+    authDomain: "pythonwithfirebase-ad416.firebaseapp.com",
+    databaseURL: "https://pythonwithfirebase-ad416.firebaseio.com",
+    projectId: "pythonwithfirebase-ad416",
+    storageBucket: "pythonwithfirebase-ad416.appspot.com",
+    messagingSenderId: "602077333641"
+  };
+  firebase.initializeApp(config);
